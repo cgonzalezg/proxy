d8b60ae5b53ba1208cbabceff6e3851de066fc01
cesar <cesargonz1984@gmail.com>
2014-08-25 23:32:50 +0200

    add configuration file

--- proxy.js
+++ proxy.js
@@ -2,6 +2,7 @@ var http = require('http'),
     httpProxy = require('http-proxy');
 var cron = require('cron');
 var tools = require('./tomcatPromise');
+var conf = require('./conf.json');
 var services = []
 var cronJob = cron.job("*/1 * * * * *", function(){
   tools.tomcat().then(function(val) {
@@ -26,9 +27,9 @@ var server = require('http').createServer(function(req, res) {
   var route = search(concant, services)
   console.log(services)
   if (route != null){
-    proxy.web(req, res, { target: 'http://localhost:8080' });
+    proxy.web(req, res, { target: conf.teamserver });
   } else {
-    proxy.web(req, res, { target: 'http://www.google.es' });
+    proxy.web(req, res, { target: conf.staging });
   }
 
 
